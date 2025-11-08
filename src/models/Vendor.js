08442c7b44d623c353380410b3931198ee2ed21c
Parames8803 <parameswaran8803@gmail.com>
2025-11-08 15:48:46 +0530

    email field added in vendor schema

--- src/models/Vendor.js
+++ src/models/Vendor.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const VendorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
+  email: { type: String },
   image: { type: String },
   rating: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
